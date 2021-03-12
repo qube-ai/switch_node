84ce91475ebbee9a9eb5630c7a8d3fe79cc52c1d
Apoorva Singh <apoorvasingh157@gmail.com>
2021-03-12 21:32:23 +0530

    More docs and code formatting

--- README.md
+++ README.md
@@ -31,7 +31,36 @@ To reset the ESP, connect `RST` to gronud for a brief moment. The chip will auto
 Tx - GPIO 1
 Rx - GPIO 3
 
-## Message Types
+## State Message
+Every time the state of the device is updated. This JSON message will be generated and sent to the gateway.
+```
+{
+  "t": 1,
+  "rs": 1,
+  "ss": 0,
+  "p": 1
+}
+```
+
+## Supported Configuration Messages
+Configuration messages are those types of messages which the device would use to update it's state.
+
+```
+{
+  "t": 1,  -> Type of the message, future proofing from
+  "rs": 0, -> Current state of relay
+  "p": 3  -> Current priority
+}
+```
+
+Since IoT core sends the most recent configuration to the device. We need to have all the information required to update the state of the device. Even if it means sending the same information over the wire again.
+
+Changes in configuration will be applied in the following order:
+1. Check the type of message (for all type 1 messages)
+2. Update the current state of relay
+3. Update the priority of the device
+
+### Old Stuff (to be removed)
 
   - **Type 1 (Node -> Gateway)** : Whenever the state of node changes. The device generates the message and sends it to the gateway. Example message:
     ```
@@ -62,6 +91,16 @@ Rx - GPIO 3
       "p": 0,  -> The priority in which the device should exist
     }
     ```
+
+## Iteracting with device
+There are two GPIO pins using which you can interact with the device.
+  1. **Relay Output Pin** - TX (GPIO 1). The relay will be driven on the basis of this output.
+     - Pin HIGH / 5V -> Relay ON -> Appliance ON
+     - Pin LOW / 0V -> Relay OFF -> Appliance OFF
+
+  2. **Switch Input Pin** - Rx (GPIO 3). The physical state of the switch is known by reading this pin.
+     - Pin reads HIGH / 5V -> Switch Off
+     - Pin reads LOW / 0V / GND -> Switch is On
 
 ## Debugging
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
     #include "namedMesh.h"
     #include <EEPROM.h>
     #include "ArduinoJson.h"
-
     #include "storage.h"
     #include <SoftwareSerial.h>
 
@@ -12,19 +11,18 @@
     #define MESH_PORT 5555
 
     /* GPIO pins in use */
-    #define RELAY_PIN 1  // Tx pin
+    #define RELAY_PIN 1   // Tx pin
     #define SWITCH_PIN 3  // Rx pin
 
-    Scheduler userScheduler;
-    namedMesh mesh;
-    int switch_pin_state = HIGH; 
-    SoftwareSerial debugSerial(0, 2); // 0 - Rx, 2 - Tx
+Scheduler userScheduler;
+namedMesh mesh;
+int switch_pin_state = HIGH;
+SoftwareSerial debugSerial(0, 2);  // 0 - Rx, 2 - Tx
 
 inline void actuateRelay(short value) {
     // Relay is active low
     digitalWrite(RELAY_PIN, value);
 }
-
 
 void switch_pin_watcher() {
     // Get the current state of switch pin
@@ -37,17 +35,20 @@ void switch_pin_watcher() {
         // Switch OPEN / OFF = HIGH
         // Switch CLOSE / ON = GND
         int relay_state = (current_state ? LOW : HIGH);
-        debugSerial.printf("Physical switch says put relay to -> %d\n", relay_state);
+        debugSerial.printf("Physical switch says put relay to -> %d\n",
+                           relay_state);
         storage::setRelayStatus(relay_state);
 
-        // Physical switch is state change is accepted only in priority 0 and 1, ignore everything else
+        // Physical switch is state change is accepted only in priority 0 and 1,
+        // ignore everything else
         int priority = 0;
         storage::getPriority(&priority);
-        if(priority == 0 || priority == 1) {
+        if (priority == 0 || priority == 1) {
             actuateRelay(relay_state);
-        }
-        else {
-            debugSerial.printf("Current priority(%d) doesn't allow for relay state change.\n", priority);
+        } else {
+            debugSerial.printf(
+                "Current priority(%d) doesn't allow for relay state change.\n",
+                priority);
         }
 
         // Create a message and send it to master
@@ -65,9 +66,9 @@ void switch_pin_watcher() {
     }
 }
 // Execute this task every 500ms
-Task switch_pin_watcher_task(500ul, TASK_FOREVER, &switch_pin_watcher);  
+Task switch_pin_watcher_task(500ul, TASK_FOREVER, &switch_pin_watcher);
 
-// Needed for painless library
+
 void receivedCallback(uint32_t from, String &msg) {
     debugSerial.printf("Received from %u msg=%s\n", from, msg.c_str());
 
@@ -79,25 +80,45 @@ void receivedCallback(uint32_t from, String &msg) {
         return;
     }
 
-    // Listen for master's address
+    // What type of message is it
     short type = doc["t"];
 
-    // Update relay state
-    if (type == 2) {
-        short relay_state = doc["rs"];
-        debugSerial.printf("Gateway says to put relay to -> %d\n", relay_state);
-        storage::setRelayStatus(relay_state);
+    // Type 1 message
+    if (type == 1) {
+        int required_relay_status = doc["rs"];
+        int required_priority = doc["p"];
 
-        // Cloud commands are only accepted in priority 1 and 2, ignore everything else
-        int priority = 0;
-        storage::getPriority(&priority);
-        if(priority == 1 || priority == 2) {
-            actuateRelay(relay_state);
+        // Fetch current state of relay
+        int current_relay_status = 0;
+        storage::getRelayStatus(&current_relay_status);
+
+        // Fetch current priority
+        int current_priority = 0;
+        storage::getPriority(&current_priority);
+
+        // update relay state if required
+        if (current_relay_status != required_relay_status) {
+            if (current_priority == 1 || current_priority == 2) {
+                storage::setRelayStatus(required_relay_status);
+                actuateRelay(required_relay_status);
+                debugSerial.println(
+                    "Relay state was updated due to new config.");
+            }
+
+            else {
+                debugSerial.printf(
+                    "Current priority(%d) doesn't allow for relay state "
+                    "change.\n",
+                    current_priority);
+            }
         }
-        else {
-            debugSerial.printf("Current priority(%d) doesn't allow for relay state change.\n", priority);
+
+        // update priority if required
+        if (current_priority != required_priority) {
+            storage::setPriority(required_priority);
+            debugSerial.println(
+                "Device priority was updated due to new config.");
         }
-        
     }
 
     else {
@@ -109,11 +130,13 @@ void newConnectionCallback(uint32_t nodeId) {
     debugSerial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
 
-void changedConnectionCallback() { debugSerial.printf("Changed connections\n"); }
+void changedConnectionCallback() {
+    debugSerial.printf("Changed connections\n");
+}
 
 void nodeTimeAdjustedCallback(int32_t offset) {
     debugSerial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),
-                  offset);
+                       offset);
 }
 
 void handleInterrupt() { debugSerial.println("Interrupt Detected"); }
@@ -134,16 +157,17 @@ void setup() {
     mesh.onChangedConnections(&changedConnectionCallback);
     mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
     debugSerial.println("Mesh properties set");
-    debugSerial.printf("Mesh properties set for ssid -> %s and pass -> %s\n", MESH_PREFIX, MESH_PASSWORD);
+    debugSerial.printf("Mesh properties set for ssid -> %s and pass -> %s\n",
+                       MESH_PREFIX, MESH_PASSWORD);
 
     // Add a task for watching the GPIO pins
     userScheduler.addTask(switch_pin_watcher_task);
     switch_pin_watcher_task.enable();
 
     // Setup GPIO pins
-    //GPIO 1 (TX) swap the pin to a GPIO.
-    pinMode(1, FUNCTION_3); 
-    //GPIO 3 (RX) swap the pin to a GPIO.
+    // GPIO 1 (TX) swap the pin to a GPIO.
+    pinMode(1, FUNCTION_3);
+    // GPIO 3 (RX) swap the pin to a GPIO.
     pinMode(3, FUNCTION_3);
     pinMode(RELAY_PIN, OUTPUT);
 
@@ -153,16 +177,16 @@ void setup() {
     // Fetch Relay state from storage
     int relay_state_from_storage = 0;
     storage::getRelayStatus(&relay_state_from_storage);
-    debugSerial.printf("Relay state as per storage -> %d\n", relay_state_from_storage);
+    debugSerial.printf("Relay state as per storage -> %d\n",
+                       relay_state_from_storage);
     actuateRelay(relay_state_from_storage);
 
-    // Set node name from storage 
+    // Set node name from storage
     char dev_id[20] = "";
     storage::getDeviceID(dev_id);
     debugSerial.printf("Device ID from storage is -> %s\n", dev_id);
     String this_node_name(dev_id);
     mesh.setName(this_node_name);
-
 
     debugSerial.println("Setup() complete");
 }
